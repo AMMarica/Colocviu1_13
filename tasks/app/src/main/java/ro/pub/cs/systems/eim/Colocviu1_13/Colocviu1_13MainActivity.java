a549ed702b31775fb8a107248fff46e605a773b8
Andreea <andreea.marica0311@gmail.com>
2020-04-13 11:47:16 +0300

    task C

--- tasks/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
+++ tasks/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
@@ -2,27 +2,40 @@ package ro.pub.cs.systems.eim.Colocviu1_13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Colocviu1_13MainActivity extends AppCompatActivity {
 
-    private Button north, south, east, west;
+    private Button north, south, east, west, navigate_to_secondary_activity;
     private EditText commands;
     Integer pressButtons = 0;
     String PRESS = "pressButtons";
+    String COMMANDS = "commands";
 
     private ButtonClickListener buttonClickListener = new ButtonClickListener();
     private class ButtonClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            pressButtons = pressButtons + 1;
-            Log.d("Increment", "Number of buttons pressed: " + String.valueOf(pressButtons));
-            commands.setText(commands.getText().toString() + " " + ((Button)view).getText().toString());
+            switch(view.getId()) {
+                case R.id.navigate_to_secondary_activity_button:
+                    Intent intent = new Intent(getApplicationContext(), Colocviu1_13SecondaryActivity.class);
+                    String commd = commands.getText().toString();
+                    intent.putExtra(COMMANDS, commd);
+                    startActivityForResult(intent, 2020);
+                    break;
+                default:
+                    pressButtons = pressButtons + 1;
+                    Log.d("Increment", "Number of buttons pressed: " + String.valueOf(pressButtons));
+                    commands.setText(commands.getText().toString() + " " + ((Button)view).getText().toString());
+                    break;
+            }
         }
     }
 
@@ -41,6 +54,9 @@ public class Colocviu1_13MainActivity extends AppCompatActivity {
         west.setOnClickListener(buttonClickListener);
 
         commands = (EditText) findViewById(R.id.instructions_edit_text);
+
+        navigate_to_secondary_activity = (Button)findViewById(R.id.navigate_to_secondary_activity_button);
+        navigate_to_secondary_activity.setOnClickListener((buttonClickListener));
 
         if (savedInstanceState != null) {
             if (savedInstanceState.containsKey(PRESS)) {
@@ -61,5 +77,15 @@ public class Colocviu1_13MainActivity extends AppCompatActivity {
             pressButtons = Integer.valueOf(savedInstanceState.getString(PRESS));
         }
         Log.d("Increment", "Number of buttons pressed: " + String.valueOf(pressButtons));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 2020) {
+            Toast.makeText(this, "The activity returned with result " + resultCode, Toast.LENGTH_LONG).show();
+            commands.setText("");
+            pressButtons = 0;
+        }
     }
 }
---
+++ tasks/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13SecondaryActivity.java
@@ -0,0 +1,51 @@
+package ro.pub.cs.systems.eim.Colocviu1_13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Colocviu1_13SecondaryActivity extends AppCompatActivity {
+
+    private TextView commandsTextView;
+    private Button registerButton, cancelButton;
+    String COMMANDS = "commands";
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.register_button:
+                    setResult(RESULT_OK, null);
+                    break;
+                case R.id.cancel_button:
+                    setResult(RESULT_CANCELED, null);
+                    break;
+            }
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colocviu1_13_secondary);
+
+        commandsTextView = (TextView)findViewById(R.id.commands_text_view);
+
+        Intent intent = getIntent();
+        if (intent != null && intent.getExtras().containsKey(COMMANDS)) {
+            String commands = intent.getStringExtra(COMMANDS);
+            commandsTextView.setText(commands);
+        }
+
+        registerButton = (Button)findViewById(R.id.register_button);
+        registerButton.setOnClickListener(buttonClickListener);
+        cancelButton = (Button)findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(buttonClickListener);
+    }
+}
